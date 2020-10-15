6080b0450eda0228bc136632680f3e63eb342a97
Francisco Alejandro Murillo Oviedo <murillo.francisco2001@gmail.com>
2020-10-14 19:40:49 -0600

    PreParcial files

---
+++ py-pre-parcial/clave_X/clave_x.py
@@ -0,0 +1,56 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma(num1,num2):
+    return num1+num2
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion(num1,num2,num3):
+    return num1*num2*num3
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista(myList):
+    suma=0
+    for element in range(len(myList)):
+        suma+=myList[element]
+    return suma
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return ""
